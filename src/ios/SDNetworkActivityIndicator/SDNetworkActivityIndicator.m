8895fd070262fefe5f502a32dbf01198203afaa6
Sachin Kumaram <sachin.kumaram@veoci.com>
2026-02-01 14:47:16 +0530

    Refactor SDNetworkActivityIndicator methods

--- src/ios/SDNetworkActivityIndicator/SDNetworkActivityIndicator.m
+++ src/ios/SDNetworkActivityIndicator/SDNetworkActivityIndicator.m
@@ -5,66 +5,82 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
+#import <UIKit/UIKit.h>
 #import "SDNetworkActivityIndicator.h"
 
-@interface SDNetworkActivityIndicator()
+@interface SDNetworkActivityIndicator ()
 {
     @private NSUInteger counter;
 }
 @end
 
-
 @implementation SDNetworkActivityIndicator
 
-+ (instancetype) sharedActivityIndicator
+#pragma mark - Private helper
+
++ (void)_setVisible:(BOOL)visible
 {
-    static id _sharedInstance = nil;
+#if !TARGET_OS_VISION
+    if (@available(iOS 13.0, *)) {
+        // Network activity indicator removed in iOS 13+
+        // Intentionally no-op
+    } else {
+        [[UIApplication sharedApplication]
+            setNetworkActivityIndicatorVisible:visible];
+    }
+#endif
+}
+
+#pragma mark - Public API
+
++ (void)setNetworkActivityIndicatorVisible:(BOOL)visible
+{
+    [self _setVisible:visible];
+}
+
++ (instancetype)sharedActivityIndicator
+{
+    static SDNetworkActivityIndicator *_sharedInstance = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         _sharedInstance = [[self alloc] init];
     });
-    
+
     return _sharedInstance;
 }
 
 - (id)init
 {
-    if ((self = [super init]))
-    {
+    if ((self = [super init])) {
         counter = 0;
     }
-
     return self;
 }
 
 - (void)startActivity
 {
-    @synchronized(self)
-    {
+    @synchronized (self) {
         counter++;
-        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+        [[self class] _setVisible:YES];
     }
 }
 
 - (void)stopActivity
 {
-    @synchronized(self)
-    {
-        if (counter > 0 && --counter == 0)
-        {
-            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+    @synchronized (self) {
+        if (counter > 0 && --counter == 0) {
+            [[self class] _setVisible:NO];
         }
     }
 }
 
 - (void)stopAllActivity
 {
-    @synchronized(self)
-    {
+    @synchronized (self) {
         counter = 0;
-        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        [[self class] _setVisible:NO];
     }
 }
 
 @end
+
